1f2ee4c106bcf790f8efbbca986eb59c4d6998a4
Muhammad Bilal <2019cs682@student.uet.edu.pk>
2026-01-12 16:12:07 +0500

    Update signup layout with new structure and styles
    Refactored the signup steps layout to include a styled container for the form, added a 'Parent Information' heading, and adjusted spacing for improved UI consistency. The children are now rendered within the new layout structure.

--- src/app/(auth)/signup/steps/layout.tsx
+++ src/app/(auth)/signup/steps/layout.tsx
@@ -1,16 +1,24 @@
 import Container from "@/components/common/Container";
-import { Heading4 } from "@/components/common/Typography";
+import { Heading2, Heading4 } from "@/components/common/Typography";
 
 const SignupStepsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Container>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-8 py-8">
           <Heading4 className="text-white">
             Create your child’s Satori account
           </Heading4>
+          <div className="bg-background-secondary p-24 rounded-[20px] flex flex-col gap-24">
+            {/* <SignupStepper /> */}
+
+            <div className="flex flex-col gap-10">
+              <Heading2 className="text-white">Parent Information</Heading2>
+
+              {children}
+            </div>
+          </div>
         </div>
-        {children}
       </Container>
     </div>
   );
