95b01184cbea62342308e12eb985d9b06f77b93b
Muhammad Bilal <2019cs682@student.uet.edu.pk>
2026-01-24 21:44:10 +0500

    Add SwiperSlider and new landing page section
    Introduced a reusable SwiperSlider component and a new LandingPageSliderAsthmaFeelsUnpredictableSection to the home page. Updated dependencies to include 'swiper'. The new section is now rendered on the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import LandingPageHeroSection from "@/components/page-components/Home/LandingPageHeroSection";
+import LandingPageSliderAsthmaFeelsUnpredictableSection from "@/components/page-components/Home/LandingPageSliderAsthmaFeelsUnpredictableSection";
 
 function Home() {
 
   return (
     <div className="block">
       <LandingPageHeroSection />
+
+      <LandingPageSliderAsthmaFeelsUnpredictableSection />
     </div>
   );
 }
---
+++ src/components/common/SwiperSlider/index.tsx
@@ -0,0 +1,9 @@
+const SwiperSlider = () => {
+    return (
+        <div>
+            <h1>SwiperSlider</h1>
+        </div>
+    );
+};
+
+export default SwiperSlider;
