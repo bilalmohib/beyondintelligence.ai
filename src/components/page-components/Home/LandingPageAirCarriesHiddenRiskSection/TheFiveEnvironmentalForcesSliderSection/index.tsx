77d3d88294379a10a9224163731d62afc45610d0
Muhammad Bilal <2019cs682@student.uet.edu.pk>
2026-02-04 13:29:02 +0500

    Use slide.title for modal image alt
    Fallback alt text for the modal image now uses slide.title instead of slide.text. The commit also applies formatting cleanup (quote style, wrapped imports, indentation and whitespace) for readability; no other functional changes were made.

--- src/components/page-components/Home/LandingPageAirCarriesHiddenRiskSection/TheFiveEnvironmentalForcesSliderSection/index.tsx
+++ src/components/page-components/Home/LandingPageAirCarriesHiddenRiskSection/TheFiveEnvironmentalForcesSliderSection/index.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import { useDispatch } from "react-redux";
@@ -9,56 +9,61 @@ import CustomModal from "@/components/common/CustomModal";
 import { Heading2 } from "@/components/common/Typography";
 import SwiperSlider from "@/components/common/SwiperSlider";
 import FiveEnvironmentalForcesModalContent from "@/components/page-components/Home/LandingPageAirCarriesHiddenRiskSection/FiveEnvironmentalForcesModalContent";
-import { ITheFiveEnvironmentalForcesSliderSectionData, theFiveEnvironmentalForcesSliderSectionData } from "@/components/page-components/Home/LandingPageAirCarriesHiddenRiskSection/TheFiveEnvironmentalForcesSliderSection/data";
+import {
+  ITheFiveEnvironmentalForcesSliderSectionData,
+  theFiveEnvironmentalForcesSliderSectionData,
+} from "@/components/page-components/Home/LandingPageAirCarriesHiddenRiskSection/TheFiveEnvironmentalForcesSliderSection/data";
 
 const TheFiveEnvironmentalForcesSliderSection = () => {
-    const dispatch = useDispatch<AppDispatch>();
-    const [selectedSlide, setSelectedSlide] = useState<ITheFiveEnvironmentalForcesSliderSectionData | null>(null);
+  const dispatch = useDispatch<AppDispatch>();
+  const [selectedSlide, setSelectedSlide] =
+    useState<ITheFiveEnvironmentalForcesSliderSectionData | null>(null);
 
-    const handleSlideSelect = (slide: ITheFiveEnvironmentalForcesSliderSectionData) => {
-        setSelectedSlide(slide);
-        if (slide.modalContent) {
-            dispatch(openModal({
-                ...slide.modalContent,
-                image: slide.modalContent.image ?? { src: slide.image, alt: slide.text },
-            }));
-        }
-    };
+  const handleSlideSelect = (
+    slide: ITheFiveEnvironmentalForcesSliderSectionData
+  ) => {
+    setSelectedSlide(slide);
+    if (slide.modalContent) {
+      dispatch(
+        openModal({
+          ...slide.modalContent,
+          image: slide.modalContent.image ?? {
+            src: slide.image,
+            alt: slide.title,
+          },
+        })
+      );
+    }
+  };
 
-    return (
-        <div className="bg-[#1A1C4E]">
-            <Container className="pt-0.5 pb-0">
-                <Heading2 className="text-white! leading-[120%]! text-left">
-                    The five environmental forces <br /> behind most asthma attacks.
-                </Heading2>
-            </Container>
+  return (
+    <div className="bg-[#1A1C4E]">
+      <Container className="pt-0.5 pb-0">
+        <Heading2 className="text-white! leading-[120%]! text-left">
+          The five environmental forces <br /> behind most asthma attacks.
+        </Heading2>
+      </Container>
 
-            <div className="h-[50px]" aria-hidden />
-            <Container className="pb-30">
-                <SwiperSlider
-                    slides={theFiveEnvironmentalForcesSliderSectionData}
-                    isModalActive
-                    onSlideSelect={handleSlideSelect}
-                    imageHeight={559}
-                />
-            </Container>
-            <CustomModal>
-                {selectedSlide?.modalContent && (
-                    <FiveEnvironmentalForcesModalContent
-                        title={selectedSlide.modalContent.title}
-                        description={selectedSlide.modalContent.description}
-                        bottomText={selectedSlide.modalContent.bottomText}
-                        image={selectedSlide.modalContent.image}
-                    />
-                )}
-            </CustomModal>
-        </div>
-    );
+      <div className="h-[50px]" aria-hidden />
+      <Container className="pb-30">
+        <SwiperSlider
+          slides={theFiveEnvironmentalForcesSliderSectionData}
+          isModalActive
+          onSlideSelect={handleSlideSelect}
+          imageHeight={559}
+        />
+      </Container>
+      <CustomModal>
+        {selectedSlide?.modalContent && (
+          <FiveEnvironmentalForcesModalContent
+            title={selectedSlide.modalContent.title}
+            description={selectedSlide.modalContent.description}
+            bottomText={selectedSlide.modalContent.bottomText}
+            image={selectedSlide.modalContent.image}
+          />
+        )}
+      </CustomModal>
+    </div>
+  );
 };
 export default TheFiveEnvironmentalForcesSliderSection;
-
-
-
-
-
-
